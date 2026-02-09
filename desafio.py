c17813a78d4d2821bc2ff91b983b48d38ff0c4c3
Jotapedev17 <cortesdesenhos285@gmail.com>
2026-02-09 20:32:09 -0300

    desafio sobre if else elif

---
+++ desafio.py
@@ -0,0 +1,33 @@
+CONSTANTE_BONUS = 1000
+
+# 1) Solicita ao usuário que digite seu nome
+nome_usuario = input("Digite o seu nome: ")
+
+if len(nome_usuario) == 0:
+    print("Você não digitou nada")
+    exit()
+
+elif any(caractere.isdigit() for caractere in nome_usuario):
+    print("Por favor, digite somente letras")
+    exit()
+
+else:
+    print("Nome válido")
+
+
+# 2) Solicita ao usuário que digite o valor do seu salário
+# Converte a entrada para um número de ponto flutuante
+salario_usuario = float(input("Digite o seu salario: "))
+
+# 3) Solicita ao usuário que digite o valor do bônus recebido
+# Converte a entrada para um número de ponto flutuante
+bonus_usuario = float(input("Digite o seu bonus: "))
+
+# 4) Calcule o valor do bônus final
+
+valor_do_bonus = CONSTANTE_BONUS + salario_usuario * bonus_usuario
+
+# 5) Imprime a mensagem personalizada incluindo o nome do usuário e o valor do bonus
+print(f"O usuario {nome_usuario} possui o bonus de {valor_do_bonus}")
+
+# Bônus: Quantos bugs e riscos você consegue identificar nesse programa?
