fc6634d90d8b48fb644dbafdda0d65a106634401
Jotapedev17 <cortesdesenhos285@gmail.com>
2026-02-09 14:47:19 -0300

    comitando o exercicio num 1

---
+++ exercicios.py
@@ -0,0 +1,16 @@
+# Exercício 1: Soma de Dois Números Inteiros
+
+#primeiro_numero = int(input("Digite o primeiro numero: "))
+#segundo_numero = int(input("Digite o segundo numero: "))
+
+resultado = primeiro_numero + segundo_numero
+
+print(resultado)
+
+# Exercício 2: Resto da Divisão por 5
+
+num = int(input("Digite um numero: "))
+
+resultado_resto = num % 5
+
+print("O resto da divisão por 5 é: ", resultado_resto)
