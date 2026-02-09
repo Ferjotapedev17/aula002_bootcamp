24e49885322452688e196ab478da8cf5e5b440a2
Jotapedev17 <cortesdesenhos285@gmail.com>
2026-02-09 17:56:34 -0300

    Adicionando o exercicio numero 4

--- exercicios.py
+++ exercicios.py
@@ -12,12 +12,17 @@ import math
 # 2. Crie um programa que receba um número do usuário e calcule o resto da divisão desse número por 5.
 # 3. Desenvolva um programa que multiplique dois números fornecidos pelo usuário e mostre o resultado.
 
-primeiro_numero = int(input("Digite o primeiro numero: "))
-segundo_numero = int(input("Digite o segundo numero: "))
-resultado = primeiro_numero * segundo_numero
-print(f"O resultado da conta é {resultado}")
+#primeiro_numero = int(input("Digite o primeiro numero: "))
+#segundo_numero = int(input("Digite o segundo numero: "))
+#resultado = primeiro_numero * segundo_numero
+#print(f"O resultado da conta é {resultado}")
 
 # 4. Faça um programa que peça dois números inteiros e imprima a divisão inteira do primeiro pelo segundo.
+
+primeiro_numero = int(input("Digite o primeiro numero inteiro: "))
+segundo_numero = int(input("Digite o segundo numero inteiro: "))
+divisao_dos_numeros = primeiro_numero // segundo_numero
+print(f"A divisao dos numeros inteiros é: {divisao_dos_numeros}")
 # 5. Escreva um programa que calcule o quadrado de um número fornecido pelo usuário.
 
 # #### Números de Ponto Flutuante (`float`)
